bd86d39a498abacecc409d3406ac507323e913c2
rahimshahad <rahim.shahad2@gmail.com>
2021-09-23 18:46:10 -0400

    add seed data to database

--- yard_sale_api/db/seeds.rb
+++ yard_sale_api/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+electronics = Category.create(name: "electronics")
+furniture = Category.create(name: "furniture")
+clothing = Category.create(name: "clothing")
+toiletries = Category.create(name: "toiletries")
+artwork = Category.create(name: "artwork")
+kitchenware = Category.create(name: "kitchenware")
+
+Item.create([
+    {name: "Iphone 13 pro max", description: "The all new flagship iphone from Apple powered by the powerful A15 Bionic chip!", condition: "Used - Like New", price: "$1000", category_id: electronics.id},
+    {name: "Iphone 13 mini", description: "The all new iphone from Apple powered by the powerful A15 Bionic chip!", condition: "New", price: "900", category_id: electronics.id},
+    {name: "Levi's Straight Fit Jeans", description: "Light blue washed jeans perfect for most ocassions", condition: "New", price: "$40", category_id: clothing.id}
+])
