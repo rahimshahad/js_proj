151c8ac1b9383a5e0ff9f68a97177c2727740d81
rahimshahad <rahim.shahad2@gmail.com>
2021-09-25 16:03:15 -0400

    create getItems function to handle read action of fetch requests

--- yard_sale/src/itemService.js
+++ yard_sale/src/itemService.js
@@ -2,4 +2,11 @@ class ItemService{
     constructor(website){
         this.website = website
     }
+
+    // read action
+    getItems(){
+        fetch(`${this.website}/items`)
+        .then (response =>response.json())
+        .then (items => console.log(items))
+    }
 }
