9d9c161b9bfd925308090079eeac6b32e02a3584
rahimshahad <rahim.shahad2@gmail.com>
2021-09-26 05:54:40 -0400

    fix syntax error in item constructor function in item class

--- yard_sale/src/item.js
+++ yard_sale/src/item.js
@@ -1,9 +1,9 @@
 class Item{
-    constructor(name, description, condition, price, id, category_id)
+    constructor(name, description, condition, price, id, category_id){
     this.name = name
     this.description = description
     this.condition = condition
     this.price = price
     this.id = id
-    this.category_id = category_id
+    this.category_id = category_id}
 }
