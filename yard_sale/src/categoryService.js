288d6e651d8a13d57f983457535f96da30bea785
rahimshahad <rahim.shahad2@gmail.com>
2021-09-27 10:42:02 -0400

    create getCategories function to handle fetch request for reading categories

--- yard_sale/src/categoryService.js
+++ yard_sale/src/categoryService.js
@@ -2,4 +2,12 @@ class categoryService{
     constructor(website){
         this.website = website
     }
+
+    getCategories(){
+        fetch(this.website + `/categories`)
+        .then(res => res.json())
+        .then(info => {console.table(info)
+        })
+        debugger
+    }
 }
